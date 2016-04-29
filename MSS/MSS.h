e1adcb34b0da440cd64cca5bb4897007c80fa87d
Владислав <yellowamongus@outlook.com>
2016-04-29 15:00:55 +0200

    Написал говнокод для авераге

--- MSS/MSS.h
+++ MSS/MSS.h
@@ -1,2 +1,10 @@
 #pragma once
-double Average(double *measures, int n)
+#include <iostream>
+using namespace std;
+double Average(double *measures, int n) {
+	double sum = 0;
+	for (int i = 0; i < n; i++); {
+		sum += measures[i];
+	}
+	cout << sum / n << endl;
+}

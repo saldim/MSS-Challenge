215a9ebca821eda0f1bc36e9828f35e2a25f1437
Дмитрий Салихов <saldim9785@gmail.com>
2016-04-29 18:40:39 +0500

    Попытка реализации MessageBox

--- MSS/MainForm.h
+++ MSS/MainForm.h
@@ -36,6 +36,7 @@ namespace MSS {
 		}
 	private: System::Windows::Forms::ListBox^  listBox1;
 	private: System::Windows::Forms::Button^  button1;
+	private: System::Windows::Forms::Label^  label1;
 	protected:
 
 	private:
@@ -53,37 +54,56 @@ namespace MSS {
 		{
 			this->listBox1 = (gcnew System::Windows::Forms::ListBox());
 			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->SuspendLayout();
 			// 
 			// listBox1
 			// 
 			this->listBox1->FormattingEnabled = true;
-			this->listBox1->Location = System::Drawing::Point(10, 6);
+			this->listBox1->Location = System::Drawing::Point(12, 47);
 			this->listBox1->Name = L"listBox1";
 			this->listBox1->Size = System::Drawing::Size(154, 199);
 			this->listBox1->TabIndex = 0;
 			// 
 			// button1
 			// 
-			this->button1->Location = System::Drawing::Point(10, 212);
+			this->button1->Location = System::Drawing::Point(10, 252);
 			this->button1->Name = L"button1";
 			this->button1->Size = System::Drawing::Size(153, 29);
 			this->button1->TabIndex = 1;
 			this->button1->Text = L"Расчитать";
 			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MainForm::button1_Click);
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(31, 31);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(111, 13);
+			this->label1->TabIndex = 2;
+			this->label1->Text = L"Внесите измерения:";
+			this->label1->Click += gcnew System::EventHandler(this, &MainForm::label1_Click);
 			// 
 			// MainForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(284, 261);
+			this->ClientSize = System::Drawing::Size(471, 401);
+			this->Controls->Add(this->label1);
 			this->Controls->Add(this->button1);
 			this->Controls->Add(this->listBox1);
 			this->Name = L"MainForm";
 			this->Text = L"MSS";
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
+	private: System::Void label1_Click(System::Object^  sender, System::EventArgs^  e) {
+	}
+	private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
+		MessageBox("Это MessageBox", "Ok", MB_OK);
+	}
 	};
 }

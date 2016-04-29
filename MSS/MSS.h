5714fda8754da2c9e26cfaefc4bfe8338665949f
Нигаматьянов Рафис < @DESKTOP-MO2ATJH>
2016-04-29 18:33:31 +0500

    Написал  функцию сортирующая по возрастанию массив результатов измерений

--- MSS/MSS.h
+++ MSS/MSS.h
@@ -42,6 +42,21 @@ double MeanSquareError(double *measures, int n) {
 	return (stdDeviation(measures, n)/sqrt(n));
 }
 
+/**
+* Функция сортирующая по возрастанию массив результатов измерений
+* Параметры:
+* +double measures - массив измерений
+* +int n - кол-во измерений
+* Автор: Нигаматьянов Рафис
+*/
 void Sort(double *measures, int n) {
-
+	double temp;
+	for (int i = 0; i < n - 1; i++) {
+		for (int j = 0; j < n - i - 1; j++)
+			if (measures[j] > measures[j + 1]){
+                temp = measures[j];
+				measures[j] = measures[j + 1];
+				measures[j + 1] = temp;
+			}
+	}
 }

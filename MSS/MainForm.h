3c3a31d1aa46463a7c3ce29f60de3658dfae2f3b
Дмитрий Салихов <saldim9785@gmail.com>
2016-05-07 23:50:27 +0500

    Прототип окна "О программе" и изменения в формах

--- MSS/MainForm.h
+++ MSS/MainForm.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "MSS.h"
+#include "AboutForm.h"
 
 namespace MSS {
 	using namespace System;
@@ -50,6 +51,8 @@ namespace MSS {
 	private: System::Windows::Forms::DataGridViewTextBoxColumn^  Id;
 	private: System::Windows::Forms::DataGridViewTextBoxColumn^  Measure;
 	private: System::Windows::Forms::Label^  label3;
+	private: System::Windows::Forms::Button^  button1;
+	private: System::Windows::Forms::Button^  button2;
 
 
 
@@ -67,7 +70,7 @@ namespace MSS {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			System::Windows::Forms::DataGridViewCellStyle^  dataGridViewCellStyle2 = (gcnew System::Windows::Forms::DataGridViewCellStyle());
+			System::Windows::Forms::DataGridViewCellStyle^  dataGridViewCellStyle1 = (gcnew System::Windows::Forms::DataGridViewCellStyle());
 			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->MeanLevelCB = (gcnew System::Windows::Forms::ComboBox());
 			this->label2 = (gcnew System::Windows::Forms::Label());
@@ -78,6 +81,8 @@ namespace MSS {
 			this->Id = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
 			this->Measure = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
 			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->CanvasPB))->BeginInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->MeasureGV))->BeginInit();
 			this->SuspendLayout();
@@ -95,7 +100,7 @@ namespace MSS {
 			// 
 			this->MeanLevelCB->FormattingEnabled = true;
 			this->MeanLevelCB->Items->AddRange(gcnew cli::array< System::Object^  >(2) { L"0.01", L"0.05" });
-			this->MeanLevelCB->Location = System::Drawing::Point(229, 23);
+			this->MeanLevelCB->Location = System::Drawing::Point(184, 36);
 			this->MeanLevelCB->Name = L"MeanLevelCB";
 			this->MeanLevelCB->Size = System::Drawing::Size(111, 21);
 			this->MeanLevelCB->TabIndex = 5;
@@ -104,7 +109,7 @@ namespace MSS {
 			// label2
 			// 
 			this->label2->AutoSize = true;
-			this->label2->Location = System::Drawing::Point(226, 7);
+			this->label2->Location = System::Drawing::Point(181, 20);
 			this->label2->Name = L"label2";
 			this->label2->Size = System::Drawing::Size(123, 13);
 			this->label2->TabIndex = 6;
@@ -113,7 +118,7 @@ namespace MSS {
 			// SysErrLabel
 			// 
 			this->SysErrLabel->AutoSize = true;
-			this->SysErrLabel->Location = System::Drawing::Point(226, 47);
+			this->SysErrLabel->Location = System::Drawing::Point(181, 60);
 			this->SysErrLabel->Name = L"SysErrLabel";
 			this->SysErrLabel->Size = System::Drawing::Size(171, 39);
 			this->SysErrLabel->TabIndex = 7;
@@ -124,7 +129,7 @@ namespace MSS {
 			// CanvasPB
 			// 
 			this->CanvasPB->BackColor = System::Drawing::Color::White;
-			this->CanvasPB->Location = System::Drawing::Point(229, 111);
+			this->CanvasPB->Location = System::Drawing::Point(184, 125);
 			this->CanvasPB->Name = L"CanvasPB";
 			this->CanvasPB->Size = System::Drawing::Size(205, 204);
 			this->CanvasPB->TabIndex = 8;
@@ -155,15 +160,15 @@ namespace MSS {
 			this->MeasureGV->MultiSelect = false;
 			this->MeasureGV->Name = L"MeasureGV";
 			this->MeasureGV->RowHeadersBorderStyle = System::Windows::Forms::DataGridViewHeaderBorderStyle::Single;
-			dataGridViewCellStyle2->Alignment = System::Windows::Forms::DataGridViewContentAlignment::MiddleLeft;
-			dataGridViewCellStyle2->BackColor = System::Drawing::Color::White;
-			dataGridViewCellStyle2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 8.25F, System::Drawing::FontStyle::Regular,
+			dataGridViewCellStyle1->Alignment = System::Windows::Forms::DataGridViewContentAlignment::MiddleLeft;
+			dataGridViewCellStyle1->BackColor = System::Drawing::Color::White;
+			dataGridViewCellStyle1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 8.25F, System::Drawing::FontStyle::Regular,
 				System::Drawing::GraphicsUnit::Point, static_cast<System::Byte>(204)));
-			dataGridViewCellStyle2->ForeColor = System::Drawing::SystemColors::WindowText;
-			dataGridViewCellStyle2->SelectionBackColor = System::Drawing::SystemColors::Highlight;
-			dataGridViewCellStyle2->SelectionForeColor = System::Drawing::SystemColors::HighlightText;
-			dataGridViewCellStyle2->WrapMode = System::Windows::Forms::DataGridViewTriState::True;
-			this->MeasureGV->RowHeadersDefaultCellStyle = dataGridViewCellStyle2;
+			dataGridViewCellStyle1->ForeColor = System::Drawing::SystemColors::WindowText;
+			dataGridViewCellStyle1->SelectionBackColor = System::Drawing::SystemColors::Highlight;
+			dataGridViewCellStyle1->SelectionForeColor = System::Drawing::SystemColors::HighlightText;
+			dataGridViewCellStyle1->WrapMode = System::Windows::Forms::DataGridViewTriState::True;
+			this->MeasureGV->RowHeadersDefaultCellStyle = dataGridViewCellStyle1;
 			this->MeasureGV->RowHeadersVisible = false;
 			this->MeasureGV->RowHeadersWidthSizeMode = System::Windows::Forms::DataGridViewRowHeadersWidthSizeMode::DisableResizing;
 			this->MeasureGV->RowTemplate->Resizable = System::Windows::Forms::DataGridViewTriState::False;
@@ -198,17 +203,39 @@ namespace MSS {
 			// label3
 			// 
 			this->label3->AutoSize = true;
-			this->label3->Location = System::Drawing::Point(226, 95);
+			this->label3->Location = System::Drawing::Point(181, 109);
 			this->label3->Name = L"label3";
 			this->label3->Size = System::Drawing::Size(48, 13);
 			this->label3->TabIndex = 11;
 			this->label3->Text = L"График:";
 			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(18, 321);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(147, 27);
+			this->button1->TabIndex = 12;
+			this->button1->Text = L"О программе";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MainForm::button1_Click);
+			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(18, 354);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(147, 27);
+			this->button2->TabIndex = 13;
+			this->button2->Text = L"Справочная информация";
+			this->button2->UseVisualStyleBackColor = true;
+			this->button2->Click += gcnew System::EventHandler(this, &MainForm::button2_Click);
+			// 
 			// MainForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(458, 337);
+			this->ClientSize = System::Drawing::Size(433, 395);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->button1);
 			this->Controls->Add(this->label3);
 			this->Controls->Add(this->SolveButton);
 			this->Controls->Add(this->MeasureGV);
@@ -229,7 +256,7 @@ namespace MSS {
 #pragma endregion
 	private: void UnderConstructionBox()
 	{
-		MessageBox::Show(this, "Under construction", "Информация");
+		MessageBox::Show(this, "Функция будет доступна в следующих версиях приложения", "Информация");
 	}	
 
 	private: System::Void SolveButton_Click(System::Object^  sender, System::EventArgs^  e) {
@@ -322,5 +349,12 @@ namespace MSS {
 		draw->DrawLine(Pens::Black, 10, 180, 180, 180);
 		CanvasPB->Image = bmp;
 	}
+private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
+	AboutForm^ form = gcnew AboutForm();
+	form->Show();
+}
+private: System::Void button2_Click(System::Object^  sender, System::EventArgs^  e) {
+	UnderConstructionBox();
+}
 };
 }
